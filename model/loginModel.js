4f0cd0002edea55b1431f19c6c9fad76b5e4358b
mac <mac@Blossom-MacBook-Air.local>
2023-04-12 00:51:04 +0100

    added login model

--- model/loginModel.js
+++ model/loginModel.js
@@ -1,16 +1,16 @@
-// const Sequelize = require("sequelize");
-// const sequelize = require('../config/database.config')
+const Sequelize = require("sequelize");
+const sequelize = require('../config/database.config')
 
-// const Login = sequelize.define('Login', {
-//     email: {
-//         type: Sequelize.STRING,
-//          allowNull: false,
-//          unique: true,
-//      },
-//      password: {
-//         type: Sequelize.STRING,
-//         allowNull: false,
-//      }
-// })
+const Login = sequelize.define('Login', {
+    email: {
+        type: Sequelize.STRING,
+         allowNull: false,
+         unique: true,
+     },
+     password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+     }
+})
 
-// module.exports = Login 
+module.exports = Login 
